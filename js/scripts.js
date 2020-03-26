72e8ba187f2e13f3762a71c9a797daf90418712e
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-03-26 11:49:18 -0700

    add photos and fixed toggle feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,11 @@
-// Back end ------>
-var snakes = 0;
-var whales= 1;
-var bats= 2;
-var results= [];
-
-
-
-
-
-
-
-// Front End------->
 $(document).ready(function(){
-  $("#wiki").submit(function(event) {
-    event.preventDefault();
-  var animals1 = $("input:radio[name=animals1]:checked").val();
-  var animals2 = $("input:radio[name=animals2]:checked").val();
-  var aniamls3= $("input:radio[name=animals3]:checked").val();
+  $(".clickable1").click(function(){
+    $("#snakes").toggle();
+  })
+  $(".clickable2").click(function(){
+    $("#whales").toggle();
+  })
+  $(".clickable3").click(function(){
+    $("#bats").toggle();
+  })
+});
