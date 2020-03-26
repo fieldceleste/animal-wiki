685555ceb8dea9b922f1aad542af6855b1d6c423
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-03-26 11:22:38 -0700

    add content to page and div ids

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-var animals = $("input:radio[name=flavor]:checked").val();
+// Back end ------>
+var snakes = 0;
+var whales= 1;
+var bats= 2;
+var results= [];
+
+
+
+
+
+
+
+// Front End------->
+$(document).ready(function(){
+  $("#wiki").submit(function(event) {
+    event.preventDefault();
+  var animals1 = $("input:radio[name=animals1]:checked").val();
+  var animals2 = $("input:radio[name=animals2]:checked").val();
+  var aniamls3= $("input:radio[name=animals3]:checked").val();
